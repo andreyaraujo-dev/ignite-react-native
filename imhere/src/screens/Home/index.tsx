f05ce481bd26f22ed7feda6c2db8811129a7c99f
Andrey Araújo <jaab.andrey@gmail.com>
2023-01-04 21:34:39 -0300

    adicionando componente de participantes

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,16 +1,29 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
+  function handleParticipantAdd() {}
+
+  function handleParticipantRemove() {}
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Terça, 3 de Janeiro de 2023</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant name="Andrey" onRemove={handleParticipantRemove} />
+      <Participant name="Carol" onRemove={handleParticipantRemove} />
     </View>
   );
 }
